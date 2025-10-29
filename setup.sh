6e5d1e2fba5eac16fda93db431c4388a4c8bcfff
Albert Cao <123220164+caoalb12@users.noreply.github.com>
2025-10-29 10:01:16 -0400

    Refactor setup.sh for Python environment setup
    Updated setup script to install Python packages in a virtual environment and removed deprecated steps.

--- setup.sh
+++ setup.sh
@@ -3,20 +3,36 @@
 set -euo pipefail
 
 echo "Step 1: Updating apt packages"
-sudo apt update -y
+sudo apt update
+sudo apt install python3-pip
+sudo pip install --upgrade pip
+sudo apt install python3.10-venv
 
-echo "Step 2: Installing pip"
-sudo curl -fsSL https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
-sudo python3 /tmp/get-pip.py
+cd /local/repository
 
-echo "Step 3: Installing PyTorch and NumPy"
-sudo pip3 install --upgrade pip
-sudo pip3 install torch numpy transformers
+echo "Step 2: Activating virtual environment"
+sudo python3 -m venv env
+source env/bin/activate
 
-echo "Step 4: Installing NVIDIA utilities and driver"
-sudo apt install -y nvidia-utils-580 nvidia-driver-580
+echo "Step 3: Installing torch and transformers"
+pip install torch
+pip install transformers
 
-sudo rm -f /tmp/get-pip.py
+# echo "Step 1: Updating apt packages"
+# sudo apt update -y
+
+# echo "Step 2: Installing pip"
+# sudo curl -fsSL https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+# sudo python3 /tmp/get-pip.py
+
+# echo "Step 3: Installing PyTorch and NumPy"
+# sudo pip3 install --upgrade pip
+# sudo pip3 install torch numpy transformers
+
+# echo "Step 4: Installing NVIDIA utilities and driver"
+# sudo apt install -y nvidia-utils-580 nvidia-driver-580
+
+# sudo rm -f /tmp/get-pip.py
 
 # echo "Step 5: Rebooting system"
 # sudo reboot
