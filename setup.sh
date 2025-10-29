c7f61bd405509109383062a8ea8e7101a733f161
Albert Cao <caoalb@umich.edu>
2025-10-29 12:07:55 -0400

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -4,14 +4,9 @@ set -Eeuo pipefail
 
 echo "Step 1: Updating apt packages"
 sudo apt update -y
-sudo apt install -y python3-pip python3.10-venv
-sudo apt install -y nvidia-driver-550
+sudo apt install -y python3-pip python3.10-venv git-lfs nvidia-driver-550
 sudo modprobe nvidia
 sudo modprobe nvidia_uvm
-
-echo "NVIDIA DEBUGGING PURPOSES ONLY:"
-lsmod | grep nvidia
-echo "-------------------------------"
 
 nvidia-smi
 
@@ -25,3 +20,8 @@ echo "Step 3: Installing torch and transformers"
 pip install --upgrade pip
 pip install torch
 pip install transformers
+
+echo "Step 4: Other Startup Commands"
+mv profile.py temp.py # Temporary fix for profile.py issue
+git lfs pull # Pulling router.pt
+echo "FINISHED"
