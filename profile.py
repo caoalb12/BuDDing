5fd683b61b6223b63d3503e5f008b710193ea0d3
Albert Cao <123220164+caoalb12@users.noreply.github.com>
2025-11-12 20:26:18 -0500

    Comment out chmod command for setup.sh
    Comment out the chmod command for setup.sh execution.

--- profile.py
+++ profile.py
@@ -28,7 +28,7 @@ elif params.cluster == "utah.cloudlab.us":
 node.hardware_type = 'ibm8335'
 
 # Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="chmod +x /local/repository/setup.sh"))
+# node.addService(pg.Execute(shell="sh", command="chmod +x /local/repository/setup.sh"))
 node.addService(pg.Execute(shell="sh", command="/local/repository/setup.sh"))
 
 # Print the RSpec to the enclosing page.
